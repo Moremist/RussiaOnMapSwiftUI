d917e1f85f55a9ecf68fe1461d94e1026a5f5e45
Moremist <rbest15@rambler.ru>
2021-11-06 22:05:02 +0300

    Initial Commit

---
+++ MapSwiftUITest/MapSwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSwiftUITestApp.swift
+//  MapSwiftUITest
+//
+//  Created by Roman Kuzmich on 06.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapSwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
